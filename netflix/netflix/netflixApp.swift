5651bdb145e5701208efc4208d3e9f7bf0f741fc
kris-randen <kris.randen@gmail.com>
2022-06-01 01:53:02 +0530

    Created Xcode project for netflix clone.

---
+++ netflix/netflix/netflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflixApp.swift
+//  netflix
+//
+//  Created by Krishnaswami Rajendren on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct netflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
